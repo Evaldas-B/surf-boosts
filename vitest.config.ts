8c2f83b1a3ccc1e6a5f0eadf82c664b74aa7ca01
evaldas-b <evaldas.bendoraitis@proton.me>
2024-07-20 19:03:54 +0300

    Add Playwright

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./tests/setup.ts",
+    setupFiles: "./vitest-setup.ts",
     mockReset: true,
     restoreMocks: true,
     include: ["**/*.test.ts", "**/*.test.tsx"],
