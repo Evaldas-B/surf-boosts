38fae62778617c8bbf24a04033e7ba07f3084ad6
evaldas-b <evaldas.bendoraitis@proton.me>
2024-07-21 09:35:51 +0300

    Configure TailwindCss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./assets/**/*.tsx",
+    "./entrypoints/**/*.tsx",
+    "./components/**/*.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
