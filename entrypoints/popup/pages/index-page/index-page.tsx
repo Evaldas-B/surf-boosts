af202a8017123adaacdcc8debc70ab5a6a3f8bea
evaldas-b <evaldas.bendoraitis@proton.me>
2024-08-12 14:28:53 +0300

    Add message when there are no boosts

--- entrypoints/popup/pages/index-page/index-page.tsx
+++ entrypoints/popup/pages/index-page/index-page.tsx
@@ -1,16 +1,42 @@
 import useStorage from "@/utils/storage/useStorage"
 import { uniq } from "lodash-es"
 import BoostPack from "./BoostPack"
+import { Button } from "@mantine/core"
+import { createId } from "@paralleldrive/cuid2"
 
 export default function IndexPage() {
   const [boosts] = useStorage("BOOSTS")
   const packs = uniq(boosts.map((boost) => boost.pack)).sort()
+  const [_navigation, setNavigation] = useStorage("NAVIGATION")
 
   return (
     <div className="mt-3 flex flex-col gap-3">
-      {packs.map((pack) => (
-        <BoostPack key={pack} name={pack} />
-      ))}
+      {packs.length > 0 &&
+        packs.map((pack) => <BoostPack key={pack} name={pack} />)}
+
+      {!packs.length && (
+        <div className="mt-10 text-center text-lg">
+          No Boosts installed. You can{" "}
+          <Button
+            size="compact-sm"
+            onClick={() =>
+              setNavigation({
+                path: "/boost",
+                props: { boostId: createId() },
+              })
+            }
+          >
+            create
+          </Button>{" "}
+          your own boosts or{" "}
+          <Button
+            size="compact-sm"
+            onClick={() => setNavigation({ path: "/boosts-repo" })}
+          >
+            install public boosts
+          </Button>
+        </div>
+      )}
     </div>
   )
 }
