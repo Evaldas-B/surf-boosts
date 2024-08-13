3295a4a54db64c8967dc8d438b839c3eff6af49c
evaldas-b <evaldas.bendoraitis@proton.me>
2024-08-13 10:24:44 +0300

    Move to injecting CSS from Service Worker

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,4 +1,3 @@
-import { Boost } from "@/utils/storage/boosts"
 import storage from "@@/utils/storage/storage"
 import { matchPattern } from "browser-extension-url-match"
 
@@ -16,27 +15,75 @@ function injectScripts(tabId: number, scripts: string[]) {
   }
 }
 
-async function injectJs(tabId: number, tabUrl: string) {
+async function getBoostsToInject(tabUrl: string) {
   const boosts = await storage.BOOSTS.getValue()
   const enabledBoostsIds = await storage.ENABLED_BOOSTS_IDS.getValue()
   const disabledPacks = await storage.DISABLED_PACKS.getValue()
 
-  const scripts = boosts
-    .filter(
-      (b): b is Omit<Boost, "javascript"> & { javascript: string } =>
-        typeof b.javascript === "string" &&
-        enabledBoostsIds.includes(b.id) &&
-        matchPattern(b.matchPatterns).assertValid().match(tabUrl) &&
-        !disabledPacks.includes(b.pack),
-    )
+  const boostsToInject = boosts.filter(
+    (b) =>
+      enabledBoostsIds.includes(b.id) &&
+      matchPattern(b.matchPatterns).assertValid().match(tabUrl) &&
+      !disabledPacks.includes(b.pack),
+  )
+
+  return boostsToInject
+}
+
+async function injectJs(tabId: number, tabUrl: string) {
+  const boostsToInject = await getBoostsToInject(tabUrl)
+  const scripts = boostsToInject
     .map((b) => b.javascript)
+    .filter((js) => js !== undefined)
 
   if (!scripts?.length) return
   injectScripts(tabId, scripts)
 }
 
+async function injectCss(tabId: number, tabUrl: string) {
+  const boostsToInject = await getBoostsToInject(tabUrl)
+  const styles = boostsToInject
+    .map((b) => b.css)
+    .filter((css) => css !== undefined)
+    .join("\n")
+    .trim()
+
+  if (!styles?.length) return
+
+  chrome.scripting.executeScript({
+    target: { tabId },
+    args: [styles],
+    func: (styles) => {
+      document.getElementById("surf-boosts")?.remove()
+      const style = document.createElement("style")
+      style.id = "surf-boosts"
+      style.textContent = styles
+      document.head.appendChild(style)
+    },
+  })
+}
+
+function runningOnBrowserPages(tabUrl: string) {
+  const browserInternalSchemes = [
+    "chrome://",
+    "edge://",
+    "about:", // Firefox
+    "safari-resource://",
+    "opera://",
+    "vivaldi://",
+  ]
+
+  return browserInternalSchemes.some((scheme) => tabUrl.startsWith(scheme))
+}
+
 export default defineBackground(() => {
   chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === "complete" && tab.url) injectJs(tabId, tab.url)
+    if (!tab.url) return
+    if (runningOnBrowserPages(tab.url)) return
+
+    if (changeInfo.status === "complete") {
+      injectJs(tabId, tab.url)
+      injectCss(tabId, tab.url)
+    }
   })
 })
